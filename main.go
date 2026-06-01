ee1d994d7a41c85b6ac433c7cf7e56adb0a53f54
Super Cool Programmer <golisevpavel122@gmail.com>
2026-06-01 13:59:22 +0700

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"sync"
+    "log"
+    "net/http"
+    "sync"
+    "time" // Добавляем пакет для таймера
 
-	"github.com/gorilla/websocket"
+    "github.com/gorilla/websocket"
 )
 
 var upgrader = websocket.Upgrader{CheckOrigin: func(r *http.Request) bool { return true }}
@@ -13,36 +14,46 @@ var clients = make(map[*websocket.Conn]bool)
 var mu sync.Mutex
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
-	ws, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer ws.Close()
+    ws, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        return // Лучше не делать log.Fatal внутри обработчика, иначе сервер упадет при ошибке клиента
+    }
+    defer ws.Close()
 
-	mu.Lock()
-	clients[ws] = true
-	mu.Unlock()
+    mu.Lock()
+    clients[ws] = true
+    mu.Unlock()
 
-	for {
-		_, msg, err := ws.ReadMessage()
-		if err != nil {
-			mu.Lock()
-			delete(clients, ws)
-			mu.Unlock()
-			break
-		}
-		// Пересылаем сообщение всем остальным подключенным клиентам
-		mu.Lock()
-		for client := range clients {
-			if client != ws {
-				client.WriteMessage(websocket.BinaryMessage, msg)
-			}
-		}
-		mu.Unlock()
-	}
+    for {
+        _, msg, err := ws.ReadMessage()
+        if err != nil {
+            mu.Lock()
+            delete(clients, ws)
+            mu.Unlock()
+            break
+        }
+        mu.Lock()
+        for client := range clients {
+            if client != ws {
+                client.WriteMessage(websocket.BinaryMessage, msg)
+            }
+        }
+        mu.Unlock()
+    }
 }
 
 func main() {
-	http.HandleFunc("/ws", handleConnections)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    // --- ЭТА ЧАСТЬ НЕ ДАЕТ СЕРВЕРУ ЗАСНУТЬ ---
+    go func() {
+        for {
+            // Замени на свой домен Render
+            http.Get("https://sere-wb5r.onrender.com") 
+            time.Sleep(10 * time.Minute) 
+        }
+    }()
+    // ----------------------------------------
+
+    http.HandleFunc("/ws", handleConnections)
+    log.Println("Server started on :8080") // Для логов
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
